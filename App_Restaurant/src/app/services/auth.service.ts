c39b6a40ab360b8a1e8d23f60ccbcb7166bcdf0c
skiiing73 <corentindegouey@gmail.com>
2025-01-07 14:10:30 +0100

    modif plats

--- App_Restaurant/src/app/services/auth.service.ts
+++ App_Restaurant/src/app/services/auth.service.ts
@@ -20,6 +20,7 @@ export class AuthService {
       tap(() => {
         localStorage.setItem('isAuthenticated', 'true');
         localStorage.setItem('username', username);
+        
       }),
       catchError((error) => {
         console.error('Login error', error);
